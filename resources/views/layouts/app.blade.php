ff84645a83c10df5c6d498d3f1725892301dab6b
ryota.fujimoto@rakuten.com <ec2-user@ip-172-31-22-125.us-east-2.compute.internal>
2018-05-31 04:56:08 +0000

    add column

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,12 +2,10 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>MessageBoard</title>
+        <title>Tasklist</title>
     </head>
 
     <body>
-        
-        @include('commons.error_tasks')
         @yield('content')
     </body>
 </html>
